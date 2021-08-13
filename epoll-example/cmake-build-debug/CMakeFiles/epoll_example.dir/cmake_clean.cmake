91761d7811849728f4a9c5a11c00f6b35e8c3475
kevin-davis <qinkai1994@qq.com>
2021-08-13 15:43:07 +0800

    add epoll-example

---
+++ epoll-example/cmake-build-debug/CMakeFiles/epoll_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epoll_example.dir/epoll.c.o"
+  "epoll_example"
+  "epoll_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/epoll_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
