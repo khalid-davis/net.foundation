5f3207ea7ec59c3ad47df1dcdaa08f221d7ccd72
khalid-jobs <qinkai1994@qq.com>
2021-08-30 09:22:48 +0800

    finish ca tls

--- go-libp2p-tls/conn.go
+++ go-libp2p-tls/conn.go
@@ -2,6 +2,7 @@ package libp2ptls
 
 import (
 	"crypto/tls"
+	"fmt"
 
 	ci "github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -28,10 +29,18 @@ func (c *conn) LocalPrivateKey() ci.PrivKey {
 	return c.privKey
 }
 
+//
 func (c *conn) RemotePeer() peer.ID {
 	return c.remotePeer
 }
 
+// RemotePublicKey 这个东西暂时拿不到, ci.PubKey不具备中途构造的能力，所以就自己实现一个
 func (c *conn) RemotePublicKey() ci.PubKey {
 	return c.remotePubKey
+}
+
+func (c *conn) String() string {
+	out := fmt.Sprintf("Remote Peer ID: %s \n", c.RemotePeer())
+	out += fmt.Sprintf("Remote Peer Addr: %s \n", c.RemoteAddr())
+	return out
 }
--- go-libp2p-tls/transport2.go
+++ go-libp2p-tls/transport2.go
@@ -2,12 +2,20 @@ package libp2ptls
 
 import (
 	"context"
+	"crypto"
+	"crypto/rsa"
+	"crypto/subtle"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"fmt"
+	"github.com/gogo/protobuf/proto"
 	ci "github.com/libp2p/go-libp2p-core/crypto"
+	pb "github.com/libp2p/go-libp2p-core/crypto/pb"
 	"github.com/libp2p/go-libp2p-core/peer"
+
 	"github.com/libp2p/go-libp2p-core/sec"
+	"github.com/minio/sha256-simd"
 	"io/ioutil"
 	"log"
 	"net"
@@ -21,7 +29,7 @@ type CATransport struct {
 	caIdentity *CAIdentity
 
 	localPeer peer.ID
-	privKey ci.PrivKey
+	privKey   ci.PrivKey
 }
 
 func CANew(key ci.PrivKey, certFile, keyFile, caFile string) (*CATransport, error) {
@@ -62,30 +70,29 @@ func CANew(key ci.PrivKey, certFile, keyFile, caFile string) (*CATransport, erro
 
 // SecureInbound runs the TLS handshake as a server.
 func (t *CATransport) SecureInbound(ctx context.Context, insecure net.Conn) (sec.SecureConn, error) {
-	config, _ := t.caIdentity.ConfigForAny()
-	cs, err := t.handshake(ctx, tls.Server(insecure, config))
+	config, keyCh := t.caIdentity.ConfigForAny()
+	cs, err := t.handshake(ctx, tls.Server(insecure, config), keyCh)
 	if err != nil {
 		insecure.Close()
 	}
 	return cs, err
 }
-
 
 func (t *CATransport) SecureOutbound(ctx context.Context, insecure net.Conn, p peer.ID) (sec.SecureConn, error) {
 	// 这里需要设置下config.ServerName
 	addr := insecure.RemoteAddr().String()
-	config, _:= t.caIdentity.ConfigForPeer(p, addr)
-	cs, err := t.handshake(ctx, tls.Client(insecure, config))
+	config, keyCh := t.caIdentity.ConfigForPeer(p, addr)
+	cs, err := t.handshake(ctx, tls.Client(insecure, config), keyCh)
 	if err != nil {
 		insecure.Close()
 	}
 	return cs, err
 }
 
-
 func (t *CATransport) handshake(
 	ctx context.Context,
 	tlsConn *tls.Conn,
+	keyCh <-chan ci.PubKey,
 ) (sec.SecureConn, error) {
 	// There's no way to pass a context to tls.Conn.Handshake().
 	// See https://github.com/golang/go/issues/18482.
@@ -123,17 +130,17 @@ func (t *CATransport) handshake(
 		return nil, err
 	}
 
-	// Should be ready by this point, don't block.
-	//var remotePubKey ci.PubKey
-	//select {
-	//case remotePubKey = <-keyCh:
-	//default:
-	//}
-	//if remotePubKey == nil {
-	//	return nil, errors.New("go-libp2p-tls BUG: expected remote pub key to be set")
-	//}
+	//Should be ready by this point, don't block.
+	var remotePubKey ci.PubKey
+	select {
+	case remotePubKey = <-keyCh:
+	default:
+	}
+	if remotePubKey == nil {
+		return nil, errors.New("go-libp2p-tls BUG: expected remote pub key to be set")
+	}
 
-	conn, err := t.setupConn(tlsConn, nil)
+	conn, err := t.setupConn(tlsConn, remotePubKey)
 	if err != nil {
 		// if the context was canceled, return the context error
 		if ctxErr := ctx.Err(); ctxErr != nil {
@@ -141,23 +148,24 @@ func (t *CATransport) handshake(
 		}
 		return nil, err
 	}
+	fmt.Println("remoteAddr: ", conn.RemoteAddr())
 	return conn, nil
 }
 
 func (t *CATransport) setupConn(tlsConn *tls.Conn, remotePubKey ci.PubKey) (sec.SecureConn, error) {
-	//remotePeerID, err := peer.IDFromPublicKey(remotePubKey)
-	//if err != nil {
-	//	return nil, err
-	//}
+	remotePeerID, err := peer.IDFromPublicKey(remotePubKey)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("remotePeer: ", remotePeerID)
 	return &conn{
-		Conn:         tlsConn,
-		localPeer:    t.localPeer,
-		privKey:      t.privKey,
-		//remotePeer:   remotePeerID,
+		Conn:      tlsConn,
+		localPeer: t.localPeer,
+		privKey:   t.privKey,
+		remotePeer:   remotePeerID,
 		remotePubKey: remotePubKey,
 	}, nil
 }
-
 
 // CAIdentity ---------------
 type CAIdentity struct {
@@ -172,7 +180,7 @@ func NewCAIdentity(cert tls.Certificate, certPoll *x509.CertPool) (*CAIdentity, 
 			//InsecureSkipVerify: true,
 			// for server
 			ClientAuth: tls.RequireAndVerifyClientCert,
-			ClientCAs: certPoll,
+			ClientCAs:  certPoll,
 			// for client
 			RootCAs: certPoll,
 		},
@@ -184,7 +192,6 @@ func (i *CAIdentity) ConfigForAny() (*tls.Config, <-chan ci.PubKey) {
 }
 
 func (i *CAIdentity) ConfigForPeer(remote peer.ID, addr string) (*tls.Config, <-chan ci.PubKey) {
-
 
 	keyCh := make(chan ci.PubKey, 1)
 	// We need to check the peer ID in the VerifyPeerCertificate callback.
@@ -211,31 +218,116 @@ func (i *CAIdentity) ConfigForPeer(remote peer.ID, addr string) (*tls.Config, <-
 
 	// We're using InsecureSkipVerify, so the verifiedChains parameter will always be empty.
 	// We need to parse the certificates ourselves from the raw certs.
-	//conf.VerifyPeerCertificate = func(rawCerts [][]byte, _ [][]*x509.Certificate) error {
-	//	defer close(keyCh)
-	//
-	//	chain := make([]*x509.Certificate, len(rawCerts))
-	//	for i := 0; i < len(rawCerts); i++ {
-	//		cert, err := x509.ParseCertificate(rawCerts[i])
-	//		if err != nil {
-	//			return err
-	//		}
-	//		chain[i] = cert
-	//	}
-	//
-	//	pubKey, err := PubKeyFromCertChain(chain)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	if remote != "" && !remote.MatchesPublicKey(pubKey) {
-	//		peerID, err := peer.IDFromPublicKey(pubKey)
-	//		if err != nil {
-	//			peerID = peer.ID(fmt.Sprintf("(not determined: %s)", err.Error()))
-	//		}
-	//		return fmt.Errorf("peer IDs don't match: expected %s, got %s", remote, peerID)
-	//	}
-	//	keyCh <- pubKey
-	//	return nil
-	//}
+	conf.VerifyPeerCertificate = func(rawCerts [][]byte, _ [][]*x509.Certificate) error {
+		defer close(keyCh)
+
+		chain := make([]*x509.Certificate, len(rawCerts))
+		for i := 0; i < len(rawCerts); i++ {
+			cert, err := x509.ParseCertificate(rawCerts[i])
+			if err != nil {
+				return err
+			}
+			chain[i] = cert
+		}
+
+		//pubKey, err := PubKeyFromCertChain(chain)
+		// todo kubeedge里面是ecdsapublic
+		//rsaPublicKey := chain[0].PublicKey.(*ecdsa.PublicKey)
+		tmp := chain[0].PublicKey.(*rsa.PublicKey)
+		pubKey := &RsaPublicKey{
+			k: *tmp,
+		}
+		//if err != nil {
+		//	return err
+		//}
+		if remote != "" && !remote.MatchesPublicKey(pubKey) {
+			peerID, err := peer.IDFromPublicKey(pubKey)
+			if err != nil {
+				peerID = peer.ID(fmt.Sprintf("(not determined: %s)", err.Error()))
+			}
+			return fmt.Errorf("peer IDs don't match: expected %s, got %s", remote, peerID)
+		}
+		keyCh <- pubKey
+		return nil
+	}
 	return conf, keyCh
+}
+
+// RsaPublicKey is an rsa public key
+type RsaPublicKey struct {
+	k rsa.PublicKey
+}
+
+// Verify compares a signature against input data
+func (pk *RsaPublicKey) Verify(data, sig []byte) (bool, error) {
+	hashed := sha256.Sum256(data)
+	err := rsa.VerifyPKCS1v15(&pk.k, crypto.SHA256, hashed[:], sig)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (pk *RsaPublicKey) Type() pb.KeyType {
+	return pb.KeyType_RSA
+}
+
+// Bytes returns protobuf bytes of a public key
+func (pk *RsaPublicKey) Bytes() ([]byte, error) {
+	return MarshalPublicKey(pk)
+}
+
+func (pk *RsaPublicKey) Raw() ([]byte, error) {
+	return x509.MarshalPKIXPublicKey(&pk.k)
+}
+
+// Equals checks whether this key is equal to another
+func (pk *RsaPublicKey) Equals(k ci.Key) bool {
+	// make sure this is an rsa public key
+	other, ok := (k).(*RsaPublicKey)
+	if !ok {
+		return basicEquals(pk, k)
+	}
+
+	return pk.k.N.Cmp(other.k.N) == 0 && pk.k.E == other.k.E
+}
+
+func basicEquals(k1, k2 ci.Key) bool {
+	if k1.Type() != k2.Type() {
+		return false
+	}
+
+	a, err := k1.Raw()
+	if err != nil {
+		return false
+	}
+	b, err := k2.Raw()
+	if err != nil {
+		return false
+	}
+	return subtle.ConstantTimeCompare(a, b) == 1
+}
+
+// MarshalPublicKey converts a public key object into a protobuf serialized
+// public key
+func MarshalPublicKey(k ci.PubKey) ([]byte, error) {
+	pbmes, err := PublicKeyToProto(k)
+	if err != nil {
+		return nil, err
+	}
+
+	return proto.Marshal(pbmes)
+}
+
+// PublicKeyToProto converts a public key object into an unserialized
+// protobuf PublicKey message.
+func PublicKeyToProto(k ci.PubKey) (*pb.PublicKey, error) {
+	pbmes := new(pb.PublicKey)
+	pbmes.Type = k.Type()
+	data, err := k.Raw()
+	if err != nil {
+		return nil, err
+	}
+	pbmes.Data = data
+	return pbmes, nil
 }
